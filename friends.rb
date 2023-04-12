d86e3b24fbdb422d0329a796bce6371853a8fa68
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-04-12 16:10:53 -0600

    Add variable and code

--- friends.rb
+++ friends.rb
@@ -5,3 +5,11 @@ puts arrays[2]
 puts arrays[-1]
 
 puts arrays[0]
+
+places = ["Aruba", "Jamaica", "Bermuda", "Bahamas", "Key Largo"]
+
+places.each do |place|
+    if place.include?("B")
+        puts place
+    end
+end
